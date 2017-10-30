a6aa9cca0e8feb5ff3a355e85c48faa2139735c4
Sjones352 <sjones@tistatech.com>
2017-10-29 22:12:58 -0400

    show 500 error

--- fileServer.js
+++ fileServer.js
@@ -15,49 +15,50 @@
    'css': 'text/css'
  };
 
- function generate500error(res, errorMessage) {
-   if (errorMessage === 'Internal Server Error') {
-     res.writeHead(500);
-     res.write(`We encounted an ${errorMessage} and will not run`);
-     return;
-   }
+ function generate500error(response, errorMessage) {
+   response.writeHead(500, {"Content-Type": "text/plain"});
+
+   response.write(`Server cannot process the request: ${errorMessage}`);
+   // Log the error message for the opearations people
+   console.log(`Server cannot process the request: ${errorMessage}`);
+   // Show which function called us and which file and LINE the call was made from
+   response.end()
+   return;
  }
 
- function send404Request(res) {
+ function send404Request(response) {
    fs.readFile('public/404.html', 'UTF-8', function(err, html) {
      if (err) {
-       generate500error(errorMessage);
+       generate500error(response, "500 Internal Server Error");
      } else {
-       res.writeHead(404, {
+       response.writeHead(404, {
          'Content-type': 'text/html'
        });
-       res.end(html);
+       response.end(html);
      }
    });
  }
 
- function onRequest(req, res) {
+ function onRequest(req, response) {
    console.log(`${req.method} ${req.url}`);
 
    var pathname = url.parse(req.url).path;
-
 
    var fileName = path.join(rootDir, pathname);
 
    // Used path.extname(path) method
    var headers = {
-     'Content-type': (mimeType[path.extname(fileName).split(".").pop()]
-   };
-
+     'Content-type': String(mimeType[path.extname(fileName).split(".").pop()]
+   )};
 
    var stream = fs.createReadStream(fileName);
 
    if (req.method !== 'GET') {
-     res.writeHead(405, {
+     response.writeHead(405, {
        "Content-type": 'text/plain'
      });
-     res.write(`HTTP method ${req.method} not yet supported`);
-     res.end();
+     response.write(`HTTP method ${req.method} not yet supported`);
+     response.end();
      return;
    }
 
@@ -65,10 +66,10 @@
      fs.readFile('public/index.html', 'UTF-8', function(err, html) {
 
        if (err) {
-         generate500error(errorMessage);
+         generate500error(response);
        } else {
-         res.writeHead(200, headers);
-         res.end(html);
+         response.writeHead(200, headers);
+         response.end(html);
          return;
        }
      });
@@ -76,15 +77,15 @@
 
    stream.on('error', function(error) {
      if (error) {
-       send404Request(res);
+       send404Request(response);
      }
    });
 
    // if File exists, stream it to user
-   res.writeHead(200, headers);
-   stream.pipe(res);
+   response.writeHead(200, headers);
+   stream.pipe(response);
  };
 
 
  http.createServer(onRequest).listen(3000);
- console.log('Starting Server'); 
+ console.log('Starting Server');
