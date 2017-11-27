5616bc0eee0583a80bd039abea561af29bb63c57
Sjones352 <sjones@tistatech.com>
2017-11-26 22:58:08 -0500

    added validation for error messages

--- public/validation.js
+++ public/validation.js
@@ -2,16 +2,22 @@
 
 function validateForm() {
 
-	var username = window.loginForm.username;
-	var password = window.loginForm.password;
+	var username = document.loginForm.username;
+	var password = document.loginForm.password;
+    var username_error = document.getElementById("username_error");
+    var password_error = document.getElementById("password_error");
 
 	if (username.value == "") {
-		window.alert("Username is required");
+		username.style.border = "1px solid red";
+		username_error.textContent = "Username is required";
+		username.focus();
 		return false;
 	}
 
 	if (password.value == "") {
-		window.alert("Password is required");
+		password.style.border = "1px solid red";
+		password_error.textContent = "password is required";
+		password.focus();
 		return false;
 	}
 
@@ -22,7 +28,9 @@ function validateForm() {
 
 
 	if (username.value == "sandra@example.com" && password.value !== "letmein") {
-		window.alert("Invalid credentials");
+		password.style.border = "1px solid red";
+		password_error.innerHTML = "Invalid credentials";
+		password.focus();
 		return true;
 	}
 
