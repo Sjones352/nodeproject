6654d1d36713cc1563491d4fefd5f909c85e192c
Sjones352 <sjones@tistatech.com>
2017-11-28 22:37:19 -0500

    added functionality to login page

--- public/validation.js
+++ public/validation.js
@@ -4,8 +4,12 @@ function validateForm() {
 
 	var username = document.loginForm.username;
 	var password = document.loginForm.password;
-    var username_error = document.getElementById("username_error");
-    var password_error = document.getElementById("password_error");
+	var username_error = document.getElementById("username_error");
+	var password_error = document.getElementById("password_error");
+	var time = document.getElementById("time");
+	// var now  = new Date();
+
+	// time.textContent = "Welcome, it is now " +  now.toLocaleTimeString();
 
 	if (username.value == "") {
 		username.style.border = "1px solid red";
@@ -22,12 +26,9 @@ function validateForm() {
 	}
 
 	if (username.value == "sandra@example.com" && password.value == "letmein") {
-		window.alert("Login accepted. Welcome");
-		return true;
-	}
-
-
-	if (username.value == "sandra@example.com" && password.value !== "letmein") {
+		time.innerHTML = "Welcome, Sandra";
+		time.focus();
+	} else if (username.value == "sandra@example.com" && password.value !== "letmein") {
 		password.style.border = "1px solid red";
 		password_error.innerHTML = "Invalid credentials";
 		password.focus();
