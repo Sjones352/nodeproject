7557854a735398805641454acbc55a8ac274d7a0
Sjones352 <sjones@tistatech.com>
2017-12-14 22:38:24 -0500

    moved submit function from html

--- public/validation.js
+++ public/validation.js
@@ -9,15 +9,16 @@ function setTime() {
 }
 window.addEventListener("load", setTime);
 
-function validateForm() {
+function validateForm(evt) {
+	evt.preventDefault();
 	var username = document.loginForm.username;
 	var password = document.loginForm.password;
 	var username_error = document.getElementById("username_error");
 	var password_error = document.getElementById("password_error");
 	var time = document.getElementById("time");
 	var hide = document.getElementById('hide');
-	var form = document.getElementById('form1');
 
+	
 	if (username.value === "") {
 		username.style.border = "1px solid red";
 		username_error.textContent = "Username is required";
@@ -39,8 +40,11 @@ function validateForm() {
 
   if (username.value === "sandra@example.com" && password.value === "letmein") {
         hide.style.visibility = 'visible';
+      
+
                
     } else {
+
         password.style.border = "1px solid red";
         password_error.innerHTML = "Invalid credentials";
         password.focus();
@@ -48,4 +52,18 @@ function validateForm() {
     }
 
     return true;
+}
+
+
+function submit() {
+	var username = document.loginForm.username;
+	var password = document.loginForm.password;
+	var username_error = document.getElementById("username_error");
+	var password_error = document.getElementById("password_error");
+	var time = document.getElementById("time");
+	var hide = document.getElementById('hide');
+
+	if (validateForm(evt)) {
+	 document.getElementById("myForm").submit();
+	}
 }
